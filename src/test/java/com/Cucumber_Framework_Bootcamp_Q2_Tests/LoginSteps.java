5953cd85eb571a0f95c60351d4d6f22cbf091c78
adeelaPIIT23 <adeela_nasreenwshs@hotmail.com>
2024-08-30 23:55:16 -0400

    This is the final push - Bootcamp

--- src/test/java/com/Cucumber_Framework_Bootcamp_Q2_Tests/LoginSteps.java
+++ src/test/java/com/Cucumber_Framework_Bootcamp_Q2_Tests/LoginSteps.java
@@ -3,18 +3,29 @@ package com.Cucumber_Framework_Bootcamp_Q2_Tests;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration; // Import Duration class
+import java.time.Duration;
 import static org.junit.Assert.assertTrue;
 
 public class LoginSteps {
-    WebDriver driver = new ChromeDriver();
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Updated WebDriverWait constructor
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void setUp() {
+        // Initialize WebDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adeela\\Desktop\\Automation 2\\CHROME\\chromedriver.exe"); 
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
     @Given("User is on the login page")
     public void user_is_on_the_login_page() {
@@ -34,9 +45,14 @@ public class LoginSteps {
 
     @Then("User should be logged in successfully")
     public void user_should_be_logged_in_successfully() {
-        // Wait for a specific element that indicates successful login
         WebElement accountLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='My Account']")));
         assertTrue(accountLink.isDisplayed());
-        driver.quit();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
